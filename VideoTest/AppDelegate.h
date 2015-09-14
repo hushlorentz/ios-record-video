3e34a9ec07c22aa44b21fa489d73b08169c73aeb
Rich Halliday <r.halliday@factore.ca>
2015-09-14 12:37:59 -0400

    Initial Commit

---
+++ VideoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoTest
+//
+//  Created by Rich Halliday on 2015-09-14.
+//  Copyright (c) 2015 Factor[e]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoTest
+//
+//  Created by Rich Halliday on 2015-09-14.
+//  Copyright (c) 2015 Factor[e]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
